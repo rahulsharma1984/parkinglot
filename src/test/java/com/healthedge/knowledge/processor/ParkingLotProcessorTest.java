49a7f8444adbc4b6e7ed6f691286f2ab13cb225b
Rahul Sharma <rsharma@healthedge.com>
2018-05-11 14:11:37 -0400

    Modified POM and added a test case

--- src/test/java/com/healthedge/knowledge/processor/ParkingLotProcessorTest.java
+++ src/test/java/com/healthedge/knowledge/processor/ParkingLotProcessorTest.java
@@ -1,5 +1,13 @@
 package com.healthedge.knowledge.processor;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class ParkingLotProcessorTest {
 
+	@Test
+	public void testMethod() {
+		Assert.assertTrue(true);
+	}
 }
